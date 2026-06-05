afc7e378685dd4d54ccd73d97da772bf3764ba77
PoonamRohankar <95074577+PoonamRohankar@users.noreply.github.com>
2026-06-05 19:25:56 +0530

    Add files via upload
    Insertion sort

---
+++ InsertionSort.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class InsertionSort{
+
+    public static void main(String[] args) {
+    
+        int[] arr = {2,5,7,1};
+                    //j k
+        for(int i=1;i<arr.length;i++){
+
+            int key=arr[i];
+            int j= i-1;
+
+
+            while(j>=0 && arr[j]>key){
+                arr[j+1] = arr[j];
+                j--; 
+            }
+
+            arr[j+1] = key;
+        }
+
+        System.out.print(Arrays.toString(arr));
+
+}
+
+}
+
